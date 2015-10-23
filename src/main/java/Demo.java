41b144b9b4f9012d8c2fb93806abea1997cd7d46
Sanyo Gorbunov <sanyo.gorbunov@gmail.com>
2015-10-23 12:29:17 +0300

    Move demo properties to demo.properties resource file.

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,13 +1,19 @@
 import com.cleverdome.auth.apikeys.*;
 
+import java.io.InputStream;
 import java.rmi.RemoteException;
+import java.util.Properties;
 import javax.xml.rpc.ServiceException;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
-        String apiKey = "=BKH^a-TMM$b9(bN(;(R!wQ2G&iwoQBycLP.Cq(z1Zfm/Ay[}K2b1%b[-mn=V5Bi|G^9wv5qXjz:FK&oy+/xJ$}v$>5-s#6{xVaeF6B:s%2%_^e][CxE3Sl!HI-fLuV:";
-        String userID = "1";
-        String[] ipAddresses = new String[] { "127.0.0.1" };
+        Properties demoProps = new Properties();
+        InputStream inputStream = Demo.class.getResourceAsStream("demo.properties");
+        demoProps.load(inputStream);
+
+        String apiKey = demoProps.getProperty("apiKey");
+        String userID = demoProps.getProperty("userID");
+        String[] ipAddresses = demoProps.getProperty("ipAddresses").split(";");
 
         ApiKeyResponseMessage responseMessage = getApiKeyResponse(apiKey, userID, ipAddresses);
         if (!responseMessage.getIsSuccess()) {
